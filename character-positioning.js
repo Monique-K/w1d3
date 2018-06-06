f57f5e6e5aca8ce9fcf8d237f9a84b3d102b7b9d
Monique-K <mkhoury87@hotmail.com>
2018-06-06 16:55:40 +0000

    worked with Gerry and Frank

---
+++ character-positioning.js
@@ -0,0 +1,21 @@
+function countLetters(input) {
+ var splitLetters = input.split("");
+ var obj = {}
+
+ for (var i = 0; i < splitLetters.length; i++) {
+   if (splitLetters.includes(splitLetters[i])) {
+     var inputLetter = splitLetters[i];
+     if (!(inputLetter in obj)) {
+       if ((inputLetter !== " ")) {
+         obj[inputLetter] = [];
+         obj[inputLetter].push(i);
+       }
+     } else {
+       obj[inputLetter].push(i);
+     }
+   }
+ }
+ return obj;
+}
+
+console.log(countLetters("lighthouse in the house"));
